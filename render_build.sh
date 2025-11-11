21052d775a0707e2acf01741d42d5edbb9fd8f0a
Sanyogita Singh <gunjanjadoun02@gmail.com>
2025-11-11 18:46:18 +0530

    Fix render_build.sh to use python -m flask and install dependencies

--- render_build.sh
+++ render_build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-echo "---- Running database migrations ----"
-flask db upgrade
-echo "---- Migration complete ----"
+echo "---- Installing Dependencies ----"
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "---- Setting Flask App ----"
+export FLASK_APP=app.py
+
+echo "---- Running Database Migrations ----"
+python -m flask db upgrade
+
+echo "---- Build Complete ----"
